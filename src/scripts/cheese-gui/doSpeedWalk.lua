c03fd82c3dbab7400566feacd521a26a4804c423
atari2600tim <29287358+atari2600tim@users.noreply.github.com>
2022-09-30 14:40:32 -0400

    Update doSpeedWalk.lua

--- src/scripts/cheese-gui/doSpeedWalk.lua
+++ src/scripts/cheese-gui/doSpeedWalk.lua
@@ -1,14 +1,23 @@
+mudlet.custom_speedwalk = true
 function doSpeedWalk()
-  echo("Path we need to take: " .. table.concat(speedWalkDir, ", ") .. "\n")
-  echo("Rooms we'll pass through: " .. table.concat(speedWalkPath, ", ") .. "\n")
-  --echo("pre\n")
-  --display(speedWalkDir)
-  for _,x in ipairs(speedWalkDir) do
-    local arr = string.split(x,",")
-    for _,y in ipairs(arr) do
-      send(y)
+  --echo("Room we're coming from: " .. speedWalkFrom .. "\n")
+  --echo("Room you're going to: " .. speedWalkTo .. "\n")
+  if(getPath(speedWalkFrom, speedWalkTo)) then
+    cecho("<green>MAPPER<reset> Path we need to take: " .. table.concat(speedWalkDir, ", ") .. "\n")
+    cecho("<green>MAPPER<reset> Rooms we'll pass through: " .. table.concat(speedWalkPath, ", ") .. "\n")
+    for _,x in ipairs(speedWalkDir) do
+      local arr = string.split(x,",")
+      for _,y in ipairs(arr) do
+        send(y)
+      end
     end
-    --send(x)
+  else
+    local dest = getRoomUserData(speedWalkTo, "file")
+    if (dest ~= "") then
+      cecho("<green>MAPPER<reset> No path to "..speedWalkTo.." found, will try goto\n")
+      send("goto "..dest)
+    else
+      cecho("<green>MAPPER<reset> No path found and do not know the filename of "..speedWalkTo.."\n")
+    end
   end
-  --echo("post\n")
 end
